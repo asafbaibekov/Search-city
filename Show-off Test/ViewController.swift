1d810dffd0d34d8635c5cf9d9917ee6558e0f33c
asafall <asafall21@gmail.com>
2019-07-01 17:18:35 +0300

    Initial Commit

---
+++ Show-off Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Show-off Test
+//
+//  Created by Asaf Baibekov on 01/07/2019.
+//  Copyright © 2019 Asaf Baibekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
